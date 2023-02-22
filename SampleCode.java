afc394d59a623ed7cb8d7b42426653ab065680a1
Amol7Developer <amol0783com@gmail.com>
2023-02-22 23:03:03 +0530

    java sample code uploaded

---
+++ SampleCode.java
@@ -0,0 +1,8 @@
+public class SampleCode {
+    public static void main(String [] args){
+        int x=2;
+        int y=3;
+        int tot=x+y;
+        System.out.println("X = "+X+" Y = "+Y+" and Total = "+tot);
+    }
+}
