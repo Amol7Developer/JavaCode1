6c24d08d85b59459ebdafca03af3fa8492f22a9d
Amol7Developer <amol0783com@gmail.com>
2023-02-22 23:26:05 +0530

    java + operator example

---
+++ TypeCastingCode/JavaOperator1.java
@@ -0,0 +1,22 @@
+public class JavaOperator1 {
+    public static void main (String [] args){
+       //Java Operators
+       //Operators are used to perform operations on variables and values.
+       //In the example below, we use the + operator to add together two values:
+        int x = 100 + 50;
+        System.out.println("Java + operator to add together two values:");
+        System.out.println(x);
+
+    //it can also be used to add together a variable and a value, or a variable and another variable:
+
+         int sum1 = 100 + 50;
+         int sum2 = sum1 + 250;
+         int sum3 = sum2 + sum2;
+    System.out.println("Java + operator used to add together a variable & a value, or a variable & another variable:")
+    
+    System.out.println(sum1);
+    System.out.println(sum2);
+    System.out.println(sum3);  
+
+    }
+}
