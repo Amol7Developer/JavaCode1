1414a2694b5fa1206295ff82d744443f67189fd6
Amol7Developer <amol0783com@gmail.com>
2023-02-23 18:16:35 +0530

    Java all assignment operator used

---
+++ JavaOperator/AssignmentOperator.java
@@ -0,0 +1,63 @@
+public class AssignmentOperator {
+    public static void main (String[]args){
+
+    //Java Assignment Operators
+    //Assignment operators are used to assign values to variables.
+    //In the example below, we use the assignment operator (=) to assign the value 10 to a variable called x:
+    int x = 10;
+    System.out.println("assignment operator (=) used:");
+    System.out.println(x);
+
+    //The assignment operator (+=) adds a value to a variable:
+    x += 5;
+    System.out.println("assignment operator (+=) used:");
+    System.out.println(x);
+
+    //The assignment operator (-=) subtract a value to a variable:
+     x -= 3;
+     System.out.println("assignment operator (-=) used:");
+    System.out.println(x);
+
+    //The assignment operator (*=) Multiply a value to a variable:
+     x *= 3;
+     System.out.println("assignment operator (*=) used:");
+     System.out.println(x);
+
+    //The assignment operator (/=) div a value to a variable:
+     x /= 3;
+     System.out.println("assignment operator (/=) used:");
+     System.out.println(x);
+
+    //The assignment operator (%=) Modof a value to a variable:
+     x %= 3;
+     System.out.println("assignment operator (%=) used:");
+     System.out.println(x);
+
+    //The assignment operator (&=) a value to a variable:
+     x &= 3;
+     System.out.println("assignment operator (&=) used:");
+     System.out.println(x);
+
+     //The assignment operator (|=) a value to a variable:
+     x |= 3;
+     System.out.println("assignment operator (|=) used:");
+     System.out.println(x);
+
+    //The assignment operator (^=) a value to a variable:
+     x ^= 3;
+     System.out.println("assignment operator (^=) used:");
+     System.out.println(x);
+
+    //The assignment operator (>>=) a value to a variable:
+     x >>= 3;
+     System.out.println("assignment operator (>>=) used:");
+     System.out.println(x);
+
+    //The assignment operator (<<=) a value to a variable:
+     x <<= 3;
+     System.out.println("assignment operator (<<=) used:");
+     System.out.println(x);
+
+                  
+    }
+}
