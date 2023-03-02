bc5df8a0d7b80d7eb90a051b727ecbf442ffaede
Amol7Developer <amol0783com@gmail.com>
2023-03-02 11:52:50 +0530

    Java Case keywords example

---
+++ JavaKeywords/JavaCaseKeyword.java
@@ -0,0 +1,33 @@
+public class JavaCaseKeyword {
+  public static void main(String[] args) {
+
+    //The case keyword marks a block of code in a switch statement.
+
+
+    int day = 4;
+    System.out.println("Calculate the weekday name (day=4): ");
+    switch (day) {
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        break;
+      case 6:
+        System.out.println("Saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;
+    }
+  }
+}
