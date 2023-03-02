dfe010e6c9f4ee5dd27aebc5f778b9f9234f03dc
Amol7Developer <amol0783com@gmail.com>
2023-03-02 11:48:15 +0530

    Java Byte keywords example

---
+++ JavaKeywords/JavaByteKeyword.java
@@ -0,0 +1,8 @@
+public class JavaByteKeyword {
+  public static void main(String[] args) {
+    byte myNum = 100;
+    //The byte keyword is a data type that can store whole numbers from -128 to 127
+
+    System.out.println("The byte keyword can store whole numbers: " + myNum);  
+  }
+}
