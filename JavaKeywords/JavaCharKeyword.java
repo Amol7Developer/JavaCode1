adca02811cf54b301ac6017976cfddb5e4f5090d
Amol7Developer <amol0783com@gmail.com>
2023-03-03 12:41:03 +0530

    Java Char keyword used

---
+++ JavaKeywords/JavaCharKeyword.java
@@ -0,0 +1,16 @@
+public class JavaCharKeyword {
+  public static void main(String[] args) {
+
+    //The char keyword is a data type that is used to store a single character.
+    //A char value must be surrounded by single quotes, like 'A' or 'c'.
+
+     char myGrade = 'B';
+        System.out.println(myGrade);
+
+        //char type array prints
+        char cars[]={'a','b','c'};
+        for (int i = 0; i < cars.length; i++) {
+        System.out.println(cars[i]);
+	}
+  }
+}
