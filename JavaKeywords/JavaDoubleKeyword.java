c962190012900448db4256d361097915f4e6efee
Amol7Developer <amol0783com@gmail.com>
2023-03-04 10:10:49 +0530

    Java Double keyword used

---
+++ JavaKeywords/JavaDoubleKeyword.java
@@ -0,0 +1,9 @@
+public class JavaDoubleKeyword {
+  public static void main(String[] args) {
+
+    //The double keyword is a data type that can store fractional numbers from 1.7e−308 to 1.7e+308.
+//Note that you should end the value with a "d":
+    double myNum = 19.99d;
+    System.out.println(myNum);  
+  }
+}
