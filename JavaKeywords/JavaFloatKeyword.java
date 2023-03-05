c9b6924967ce9f7b40daba1ae679c9b4ad7f9c48
Amol7Developer <amol0783com@gmail.com>
2023-03-05 09:52:39 +0530

    Java Float keyword used

---
+++ JavaKeywords/JavaFloatKeyword.java
@@ -0,0 +1,10 @@
+public class JavaFloatKeyword {
+  public static void main(String[] args) {
+
+    //The float keyword is a data type that can store fractional numbers from 3.4e−038 to 3.4e+038.
+    //Note that you should end the value with an "f":
+    
+    float myNum = 5.75f;
+    System.out.println(myNum);  
+  }
+}
