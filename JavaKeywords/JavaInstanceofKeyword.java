c38bc161cb7c6a64a8d5a9c0098c83e2aa382167
Amol7Developer <amol0783com@gmail.com>
2023-03-06 12:13:15 +0530

    Java instanceof keyword used

---
+++ JavaKeywords/JavaInstanceofKeyword.java
@@ -0,0 +1,9 @@
+public class JavaInstanceofKeyword {  
+  public static void main(String[] args) {  
+    //The instanceof keyword checks whether an object is an instance of a specific class or an interface.
+    //The instanceof keyword compares the instance with type. The return value is either true or false.
+
+    JavaInstanceofKeyword myObj = new JavaInstanceofKeyword();  
+    System.out.println(myObj instanceof JavaInstanceofKeyword); 
+  }  
+} 
