72edddc869d3504f2ca1bdd1eb3c056f8db49690
Amol7Developer <amol0783com@gmail.com>
2023-03-06 12:32:19 +0530

    update name

--- JavaKeywords/JavaPrivateKeyword.java
+++ JavaKeywords/JavaPrivateKeyword.java
@@ -1,4 +1,4 @@
-public class Main {
+public class JavaPrivateKeyword {
   private String fname = "John";
   private String lname = "Doe";
   private String email = "john@doe.com";
@@ -8,7 +8,7 @@ public class Main {
 
 
   public static void main(String[] args) {
-    Main myObj = new Main();
+    JavaPrivateKeyword myObj = new JavaPrivateKeyword();
     System.out.println("Name: " + myObj.fname + " " + myObj.lname);
     System.out.println("Email: " + myObj.email);
     System.out.println("Age: " + myObj.age);
