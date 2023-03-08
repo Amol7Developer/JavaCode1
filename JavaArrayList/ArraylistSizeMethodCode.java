8e73ba908994352fefb6af71dd1654e14b806368
Amol7Developer <amol0783com@gmail.com>
2023-03-08 12:44:41 +0530

    Java Arraylist size() method used

---
+++ JavaArrayList/ArraylistSizeMethodCode.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+//To find out how many elements an ArrayList have, use the size method:
+public class Main { 
+  public static void main(String[] args) { 
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars.size());
+  } 
+}
