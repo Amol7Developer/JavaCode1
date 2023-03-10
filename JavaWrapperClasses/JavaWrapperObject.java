ea46932e594a98d47c89ba3cc803add9c455328d
Amol7Developer <amol0783com@gmail.com>
2023-03-10 13:39:29 +0530

    Java Wrapper Object value print

---
+++ JavaWrapperClasses/JavaWrapperObject.java
@@ -0,0 +1,12 @@
+public class JavaWrapperObject {
+    //To create a wrapper object, use the wrapper class instead of the primitive type.
+    // To get the value, you can just print the object:
+  public static void main(String[] args) {
+    Integer myInt = 5;
+    Double myDouble = 5.99;
+    Character myChar = 'A';
+    System.out.println(myInt);
+    System.out.println(myDouble);
+    System.out.println(myChar);
+  }
+}
