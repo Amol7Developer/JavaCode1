0822cb045490faf29784cb7db59dff4b10c2c0b4
Amol7Developer <amol0783com@gmail.com>
2023-03-11 12:37:21 +0530

    Java LinkedList getFirst() and getLast() methods used.

---
+++ JavaClasses/JavaLinkedListGetMethods.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class JavaLinkedListGetMethods {
+  public static void main(String[] args) {
+    LinkedList<String> cars = new LinkedList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    
+    // Use getFirst() to display the first item in the list
+    System.out.println(cars.getFirst());
+    
+     // Use getLast() to display the last item in the list
+    System.out.println(cars.getLast());
+  }
+}
