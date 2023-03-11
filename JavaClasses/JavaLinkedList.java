e237212d7a08b96e29687d806e0c84ba4d78c748
Amol7Developer <amol0783com@gmail.com>
2023-03-11 11:43:57 +0530

    Java LinkedList updated.

--- JavaClasses/JavaLinkedList.java
+++ JavaClasses/JavaLinkedList.java
@@ -1,6 +1,6 @@
 import java.util.LinkedList;
 //The LinkedList class is a collection which can contain many objects of the same type, just like the ArrayList.
-public class Main { 
+public class JavaLinkedList { 
   public static void main(String[] args) { 
     LinkedList<String> cars = new LinkedList<String>();
     cars.add("Volvo");
